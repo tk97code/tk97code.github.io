463464268e2eca3ff42c6953892ea223592371ae
tk97 <62497108+tk97code@users.noreply.github.com>
2020-04-16 08:41:49 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -24,8 +24,12 @@ socket.on('ONLINE_LIST', arrUserInfo => {
 socket.on('RIGISTER_FAIL', () => alert('Plaese choose other username'));
 
 function openStream() {
-    const config = { audio: true, video: true };
-    return navigator.mediaDevices.getUserMedia(config);
+    // const config = { audio: true, video: true };
+    // return navigator.mediaDevices.getUserMedia(config);
+    var constrains = {
+        video: { facingmode: 'user'}
+    }
+    return navigator.mediaDevices.getUserMedia(constrains);
 }
 
 function playStream(idVideoTag, stream) {
